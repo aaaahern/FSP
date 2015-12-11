ac36673ac86c217cc21d3908dc6b27a1085eedae
Yuheng Zhan <ahern.here@gmail.com>
2015-12-11 15:24:56 -0500

    add aggregationByQuality

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,12 @@ var net = require('net');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var sock = net.createConnection({port: 0x1234}, function() {
+  var sock = net.createConnection({port: 0x1234, host: "172.16.205.90"}, function() {
     console.log('connect to service');
+    sock.write('aggregationByTicker');  //send request to service
   });
   var result;
-  sock.write('Request from node');  //send request to service
+
   sock.on('data', function(data) {
     console.log(data.toString());
     result = JSON.parse(data.toString());
@@ -19,6 +20,32 @@ router.get('/', function(req, res, next) {
 
 
     res.render('index', { title: 'Daily Change By Ticker', content: result.aggregationByTicker });
+  });
+  sock.on('end', function() {
+    console.log('disconnect to service');
+  });
+  sock.on('uncaughtException', function() {
+    console.log('err');
+  });
+
+});
+
+
+
+router.get('/byQuality', function(req, res, next) {
+  var sock = net.createConnection({port: 0x1234, host: "172.16.205.90"}, function() {
+    console.log('connect to service');
+    sock.write('aggregationByQuality');  //send request to service
+  });
+  var result;
+
+  sock.on('data', function(data) {
+    console.log(data.toString());
+    result = JSON.parse(data.toString());
+    sock.end();
+
+
+    res.render('byQuality', { title: 'Daily Change By Quality', content: result.aggregationByQuality });
   });
   sock.on('end', function() {
     console.log('disconnect to service');
